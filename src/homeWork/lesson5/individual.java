776b0e3e5e18024f7fa11b7e14256499fe353a16
MilIenium <andrei.lavrinovich@t-systems.com>
2020-11-17 22:06:52 +0300

    Где коммит?

---
+++ src/homeWork/lesson5/individual.java
@@ -0,0 +1,16 @@
+package homeWork.lesson5;
+
+public class individual {
+
+
+        public static void main(String[] args) {
+                //первая задачка
+                human person = new human(20, "Andrei", "Lavrinovich", 175, 80);
+                String result = null;
+                for(int i = 0; i < 10; i++){
+                       person.equals("Andrei");
+                }
+
+        }
+
+}
